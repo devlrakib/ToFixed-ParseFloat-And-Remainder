03019841e6c98ddbb042999daabe718588e1c7b6
devlrakib <devrakib5@gmail.com>
2024-03-13 13:20:16 +0600

    first commit

---
+++ types.js
@@ -0,0 +1,24 @@
+var price = 100;
+console.log( typeof price);
+
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1+num2;
+sum = sum.toFixed(2);
+sum = parseFloat(sum);
+console.log( typeof sum);
+
+// var num1 = 0.1;
+// var num2 = 0.2;
+// var sum1 = num1+num2;
+// console.log(sum1);
+
+
+/*var num3 = 0.5;
+var num4 = 0.2;
+var sum1 = num3+num4;
+console.log(sum1);*/
+
+
+vagfol => /
+Vagsesh => %
